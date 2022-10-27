981eff892b5c3217914f4f8d5c1f2b09d30e127a
Angela <chepkoechangela514@gmail.com>
2022-10-27 12:13:29 +0300

    done

--- lib/plant.rb
+++ lib/plant.rb
@@ -1,14 +1,4 @@
+# frozen_string_literal: true
+
 class Plant
   attr_accessor :garden, :name
-
-  @@all = []
-
-  def initialize(name:)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-end
